03ae67c6d8357fb7ad366705aabb5669e9ab6990
Antony Holmes <antony.b.holmes@gmail.com>
2018-10-19 20:02:02 -0400

    Updates and bug fixes.

--- edu.columbia.rdf.matcalc.toolbox.violin.app/src/main/java/edu/columbia/rdf/matcalc/toolbox/violin/ViolinModule.java
+++ edu.columbia.rdf.matcalc.toolbox.violin.app/src/main/java/edu/columbia/rdf/matcalc/toolbox/violin/ViolinModule.java
@@ -3,14 +3,19 @@ package edu.columbia.rdf.matcalc.toolbox.violin;
 import java.awt.Desktop;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Path;
 import java.util.List;
 
 import org.jebtk.core.ColorUtils;
 import org.jebtk.core.collections.UniqueArrayList;
+import org.jebtk.core.http.Http;
+import org.jebtk.core.http.URLUtils;
+import org.jebtk.core.http.UrlBuilder;
 import org.jebtk.core.io.FileUtils;
 import org.jebtk.core.io.PathUtils;
-import org.jebtk.core.io.TempService;
+import org.jebtk.core.io.TmpService;
 import org.jebtk.core.settings.SettingsService;
 import org.jebtk.core.sys.ExternalProcess;
 import org.jebtk.graphplot.figure.series.XYSeries;
@@ -28,8 +33,7 @@ import edu.columbia.rdf.matcalc.MainMatCalcWindow;
 import edu.columbia.rdf.matcalc.toolbox.CalcModule;
 import edu.columbia.rdf.matcalc.toolbox.violin.app.ViolinIcon;
 
-public class ViolinModule extends CalcModule
-implements ModernClickListener {
+public class ViolinModule extends CalcModule implements ModernClickListener {
 
   // private static final int DEFAULT_POINTS =
   // SettingsService.getInstance().getInt("pattern-discovery.cdf.points");
@@ -37,16 +41,21 @@ implements ModernClickListener {
   // private static final List<Double> EVAL_POINTS =
   // Linspace.generate(0, 1, DEFAULT_POINTS);
 
-  private static Path SCRIPT = 
-      SettingsService.getInstance().getFile("violin.script.path"); //PathUtils.getPath("res/scripts/python/violin.py");
+  private static Path SCRIPT = SettingsService.getInstance()
+      .getFile("violin.script.path"); // PathUtils.getPath("res/scripts/python/violin.py");
 
   private MainMatCalcWindow mWindow;
 
-  private static final Logger LOG = LoggerFactory
-      .getLogger(ViolinModule.class);
+  private static final Logger LOG = LoggerFactory.getLogger(ViolinModule.class);
 
-  private static final String PYTHON = 
-      SettingsService.getInstance().getString("violin.python.interpreter");
+  private static final String PYTHON = SettingsService.getInstance()
+      .getString("violin.python.interpreter");
+
+  private static final URL PDF_URL = SettingsService.getInstance()
+      .getUrl("violin.plot.pdf.url");
+
+  private static boolean WEB_MODE = SettingsService.getInstance()
+      .getString("violin.plot.mode").equals("web");
 
   @Override
   public String getName() {
@@ -61,8 +70,7 @@ implements ModernClickListener {
         AssetService.getInstance().loadIcon(ViolinIcon.class, 24),
         "Violin Plot", "Create violin plots.");
     button.addClickListener(this);
-    mWindow.getRibbon().getToolbar("Plot").getSection("Plot")
-    .add(button);
+    mWindow.getRibbon().getToolbar("Plot").getSection("Plot").add(button);
   }
 
   @Override
@@ -79,13 +87,13 @@ implements ModernClickListener {
    *
    * @param properties the properties
    * @throws IOException Signals that an I/O exception has occurred.
-   * @throws InterruptedException 
+   * @throws InterruptedException
    */
   private void violin() throws IOException, InterruptedException {
     XYSeriesGroup groups = mWindow.getRowGroups();
 
     if (groups.getCount() == 0) {
-      MainMatCalcWindow.createGroupWarningDialog(mWindow);
+      MainMatCalcWindow.createRowGroupWarningDialog(mWindow);
 
       return;
     }
@@ -110,17 +118,21 @@ implements ModernClickListener {
       return;
     }
 
-    Path in = TempService.getInstance().generateTmpFile("txt");
+    // Write data file in specific form, two columns: Label and Value
 
-    BufferedWriter writer = FileUtils.newBufferedWriter(in);
+    Path dataFile = TmpService.getInstance().newTmpFile("txt");
+
+    BufferedWriter writer = FileUtils.newBufferedWriter(dataFile);
 
     try {
       writer.write("Label\tValue");
       writer.newLine();
-      
+
+      // Write samples in the order of the groups, thus allowing user to
+      // adjust ordering if necessary without recreating file.
       for (XYSeries group : groups) {
         List<Integer> indices = XYSeries.findRowIndices(m, group);
-      
+
         for (int i : indices) {
           writer.write(m.getRowName(i) + "\t" + m.getText(i, 0));
           writer.newLine();
@@ -130,10 +142,9 @@ implements ModernClickListener {
       writer.close();
     }
 
+    // Write out the group colors in the order given
 
-    // Make the colors file
-
-    Path colorFile = TempService.getInstance().generateTmpFile("txt");
+    Path colorFile = TmpService.getInstance().newTmpFile("txt");
 
     writer = FileUtils.newBufferedWriter(colorFile);
 
@@ -141,6 +152,7 @@ implements ModernClickListener {
       writer.write("Color");
       writer.newLine();
       for (XYSeries group : groups) {
+        // Use hex colors e.g. #FF0000
         writer.write(ColorUtils.toHtml(group.getColor()));
         writer.newLine();
       }
@@ -148,26 +160,84 @@ implements ModernClickListener {
       writer.close();
     }
 
-    // First make a table for seaborn
+    // Create a random file
+    Path out = TmpService.getInstance().newTmpFile("violin", "pdf");
 
-    Path out = TempService.getInstance().generateTmpFile("violin", "pdf");
+    if (WEB_MODE) {
+      webPdf(dataFile,
+          colorFile,
+          dialog.getShowViolin(),
+          dialog.getShowBox(),
+          dialog.getShowSwarm(),
+          dialog.getXLabel(),
+          dialog.getYLabel(),
+          out);
+    } else {
+      localPdf(dataFile,
+          colorFile,
+          dialog.getShowViolin(),
+          dialog.getShowBox(),
+          dialog.getShowSwarm(),
+          dialog.getXLabel(),
+          dialog.getYLabel(),
+          out);
+    }
 
-    ExternalProcess p = new ExternalProcess(TempService.getInstance().getTmpDir());
+    // Use default app to open PDF
+    Desktop.getDesktop().open(out.toFile());
+  }
+
+  private static void localPdf(Path tableFile,
+      Path colorFile,
+      boolean violin,
+      boolean box,
+      boolean swarm,
+      String xlabel,
+      String ylabel,
+      Path out) throws IOException, InterruptedException {
+    ExternalProcess p = new ExternalProcess(
+        TmpService.getInstance().getTmpDir());
     p.addArg(PYTHON,
-        PathUtils.toString(SCRIPT), 
-        PathUtils.toString(in), 
-        PathUtils.toString(colorFile), 
+        PathUtils.toString(SCRIPT),
+        PathUtils.toString(tableFile),
+        PathUtils.toString(colorFile),
         PathUtils.toString(out));
-    p.addParam("xlabel", dialog.getXLabel());
-    p.addParam("ylabel", dialog.getYLabel());
-    p.addParam("violinplot", dialog.getShowViolin());
-    p.addParam("boxplot", dialog.getShowBox());
-    p.addParam("swarmplot", dialog.getShowSwarm());
+    p.addParam("xlabel", xlabel);
+    p.addParam("ylabel", ylabel);
+    p.addParam("violinplot", violin);
+    p.addParam("boxplot", box);
+    p.addParam("swarmplot", swarm);
 
     System.err.println(p.toString());
-    
+
     p.run();
-    
-    Desktop.getDesktop().open(out.toFile());
+  }
+
+  private static void webPdf(Path tableFile,
+      Path colorFile,
+      boolean violin,
+      boolean box,
+      boolean swarm,
+      String xlabel,
+      String ylabel,
+      Path out) throws IOException {
+    UrlBuilder url = new UrlBuilder(PDF_URL).param("violin", violin)
+        .param("box", box).param("swarm", swarm).param("x", xlabel)
+        .param("y", ylabel);
+
+    InputStream inputStream = Http.open(url)
+        .post()
+        .multipart()
+        .addFile("data_file", tableFile)
+        .addFile("color_file", colorFile)
+        .execute()
+        .getInputStream();
+
+    try {
+      // Stream the output into the tmp PDF file
+      URLUtils.downloadFile(inputStream, out);
+    } finally {
+      inputStream.close();
+    }
   }
 }
