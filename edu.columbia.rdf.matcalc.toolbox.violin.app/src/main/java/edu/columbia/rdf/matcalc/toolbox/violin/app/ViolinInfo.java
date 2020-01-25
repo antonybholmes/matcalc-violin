2e02105e44340bf57f104fe6784aeba949740bb2
Antony Holmes <antony.b.holmes@gmail.com>
2020-01-25 16:51:20 -0500

    Updates and bug fixes.

--- edu.columbia.rdf.matcalc.toolbox.violin.app/src/main/java/edu/columbia/rdf/matcalc/toolbox/violin/app/ViolinInfo.java
+++ edu.columbia.rdf.matcalc.toolbox.violin.app/src/main/java/edu/columbia/rdf/matcalc/toolbox/violin/app/ViolinInfo.java
@@ -1,7 +1,6 @@
 package edu.columbia.rdf.matcalc.toolbox.violin.app;
 
 import org.jebtk.core.AppVersion;
-import org.jebtk.modern.AssetService;
 import org.jebtk.modern.help.GuiAppInfo;
 
 public class ViolinInfo extends GuiAppInfo {
@@ -9,8 +8,7 @@ public class ViolinInfo extends GuiAppInfo {
   public ViolinInfo() {
     super("Violin", new AppVersion(1),
         "Copyright (C) ${year} Antony Holmes",
-        AssetService.getInstance().loadIcon(ViolinIcon.class, 32),
-        AssetService.getInstance().loadIcon(ViolinIcon.class, 128));
+        new ViolinIcon());
   }
 
 }
